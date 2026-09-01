e49605a4a8f1672d2d56169f50e6e767232dc320
LandonChoe <220474@mcpsmd.net>
2026-09-01 11:08:31 -0400

    Update Easter.java

--- Easter.java
+++ Easter.java
@@ -1,4 +1,25 @@
-int y = 2026;
-int a = y mod 19;
+//Landon Choe
+// This program will calculate the date easter falls on given a year after 1583.
+import java.util.*;
+
+public class Easter{
+    public static void main(String[] args) {
 int b = y/100;
-int c = y mod 100
+int c = y % 100
+int d = b/4;
+int e = b % 4;
+int f = (b + 8)/25;
+int g = (b - f + 1)/3;
+int h =  (19 * a + b – d – g + 15) % 30;
+      int i = c/4;
+      int k = c % 4;
+      int r = (32 + 2 * e + 2 * i - h - k) % 7;
+      int m = (a + 11 * h + 22 * r)/451;
+      int n = (h + r - 7 * m + 114)/31;
+      int p = (h + r - 7 * m + 114) % 31;
+        Scanner scanner = new Scanner(System.in); //this allows you to read input from the user.
+        int y = scanner.nextInt(); // reads the next number the user types. 
+        int a = y % 19;
+        System.out.println("a = "+a);
+    }
+}
