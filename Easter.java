43160ae7ffcf894f1ea11bbdf75670534167f9ad
LandonChoe <220474@mcpsmd.net>
2026-09-02 10:45:51 -0400

    Update Easter.java

--- Easter.java
+++ Easter.java
@@ -4,13 +4,13 @@ import java.util.*;
 
 public class Easter{
     public static void main(String[] args) {
-int b = y/100;
-int c = y % 100
-int d = b/4;
-int e = b % 4;
-int f = (b + 8)/25;
-int g = (b - f + 1)/3;
-int h =  (19 * a + b – d – g + 15) % 30;
+    int b = y/100;
+    int c = y % 100
+    int d = b/4;
+    int e = b % 4;
+    int f = (b + 8)/25;
+    int g = (b - f + 1)/3;
+    int h =  (19 * a + b – d – g + 15) % 30;
       int i = c/4;
       int k = c % 4;
       int r = (32 + 2 * e + 2 * i - h - k) % 7;
